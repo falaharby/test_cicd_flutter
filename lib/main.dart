5a5ecac5fb0b6bd8c1129c32a7b4a9c346637baf
falaharby <falahhikamudin.arby@gmail.com>
2025-08-13 18:24:51 +0700

    test build

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Test Build Home Page ${const String.fromEnvironment('BASE_URL')}'),
+      home: const MyHomePage(title: 'Test Build Test Home Page ${const String.fromEnvironment('BASE_URL')}'),
     );
   }
 }
